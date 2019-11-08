0b21450d691e6545921716c7ed067168943e2818
Nick Sirek <n.sirek@hotmail.com>
2019-11-08 20:07:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string){
   if(string.toLowerCase){
     return "I can't hear you!"
   } else if(string.toUpperCase){
-      return "Yes INDEED!"
+      return "YES INDEED!"
   } else if(string === "I love you, Grandma."){
       return "I love you, too."
   }
